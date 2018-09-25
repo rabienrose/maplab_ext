91a1d8376ff4e52e1d0adc17c96f7c50074457a6
Zili Wang <283136745@qq.com>
2018-09-25 21:49:42 +0800

    succ add frame to map

--- src/main.cc
+++ src/main.cc
@@ -65,63 +65,47 @@ int main(int argc, char** argv){
         return -1;
     }
     
-    //std::vector<std::string> files;
-    //getdir(FLAGS_images_folder, files);
-    //std::cout<<files[10]<<std::endl;
-    cv::Mat img = cv::imread("/media/chamo/095d3ecf-bef8-469d-86a3-fe170aec49db/parking_lot_h/100000.jpg");
-    cv::cvtColor(img, img, cv::COLOR_BGR2GRAY);
-    std::cout<<"img size: "<<img.cols<<","<<img.rows<<std::endl;
-    
-    
-    
-    std::vector<cv::KeyPoint> keypoints;
-    //brisk::BriskFeatureDetector* detector_=new brisk::BriskFeatureDetector(70);
-//     brisk::ScaleSpaceFeatureDetector<brisk::HarrisScoreCalculator>* detector_ =
-//         new brisk::ScaleSpaceFeatureDetector<brisk::HarrisScoreCalculator>(8, 5, 70, 1000);
-//     cv::Ptr<cv::Feature2D> detector_ = cv::xfeatures2d::SurfFeatureDetector::create(100,4,3);
-//     detector_->detect(img, keypoints);
-//     std::cout<<keypoints.size()<<std::endl;
-  
+    std::vector<std::string> files;
+    getdir(FLAGS_images_folder, files);
     aslam::PinholeCamera::Ptr pinhole_A = aslam::createCamera<aslam::PinholeCamera, aslam::RadTanDistortion>(
           Eigen::Vector4d(411,411,319,190),  // intrinsics
           640, 360,  //resolution
           Eigen::Vector4d(-0.3368, 0.1009, -0.0018, 3.8804e-04)); // distortion coeffs
-//     
-//     //aslam::PinholeCamera::Ptr camera = aslam::PinholeCamera::createTestCamera();
-//     //aslam::BriskVisualPipeline* extractor=new aslam::BriskVisualPipeline(camera, true, 8, 5, 30, 1000, false, true);
-    aslam::FreakVisualPipeline* extractor= new aslam::FreakVisualPipeline(pinhole_A, false, 4, 100, 3, true, true, 1);
-    std::shared_ptr<aslam::VisualFrame> frame=extractor->processImage(img, 0);
-    std::cout<<"kps: "<<frame->getKeypointMeasurements()<<std::endl;
-    
-    
-    return 0;
     aslam::VisualPipeline::Ptr pipeline_;
     pipeline_.reset(new aslam::FreakVisualPipeline(pinhole_A, false, 4, 100, 3, true, true, 5));
     //pipeline_.reset(new aslam::BriskVisualPipeline(pinhole_A, false, 8, 5, 30, 1000, false, true));
     vi_map::VIMap* map= new vi_map::VIMap();
+    
+    aslam::Transformation trans;
+    aslam::TransformationVector T_C_B;
+    T_C_B.push_back(trans);
+    aslam::NCameraId n_camera_id_;
+    common::generateId(&n_camera_id_);
+    std::vector<std::shared_ptr<aslam::Camera>> cameras;
+    cameras.push_back(pinhole_A);
+    aslam::NCamera::Ptr cameras_(new aslam::NCamera(n_camera_id_, T_C_B, cameras, "chamo"));
+    
+    vi_map::MissionId mission_id_;
+    common::generateId(&mission_id_);
+    map->addNewMissionWithBaseframe(
+        mission_id_, aslam::Transformation(),
+        Eigen::Matrix<double, 6, 6>::Identity(), cameras_,
+        vi_map::Mission::BackBone::kViwls);
+    
     float time_stamp=0;
     for (int i=0;i<files.size();i++){
         std::cout<<files[i]<<std::endl;
         cv::Mat img = cv::imread(files[i]);
-        std::cout<<"img size: "<<img.cols<<","<<img.rows<<std::endl;
+        cv::cvtColor(img, img, cv::COLOR_BGR2GRAY);
         std::shared_ptr<aslam::VisualFrame> frame=pipeline_->processImage(img, time_stamp);
         std::cout<<frame->getKeypointMeasurement(0)<<std::endl;
         pose_graph::VertexId vertex_id_;
-        vi_map::MissionId mission_id_;
-        aslam::NFramesId n_frame_id_;
-        aslam::NCameraId n_camera_id_;
         common::generateId(&vertex_id_);
+        aslam::NFramesId n_frame_id_;
         common::generateId(&n_frame_id_);
-        common::generateId(&mission_id_);
-        common::generateId(&n_camera_id_);
         std::cout<<vertex_id_<<std::endl;
-        aslam::Transformation trans;
-        aslam::TransformationVector T_C_B;
-        T_C_B.push_back(trans);
-        std::vector<std::shared_ptr<aslam::Camera>> cameras;
-        cameras.push_back(pinhole_A);
-        aslam::NCamera::Ptr cameras_(new aslam::NCamera(n_camera_id_, T_C_B, cameras, "chamo"));
         aslam::VisualNFrame::Ptr visual_n_frame(new aslam::VisualNFrame(n_frame_id_, cameras_));
+        visual_n_frame->setFrame(0, frame);
         vi_map::Vertex* map_vertex = new vi_map::Vertex(vertex_id_, visual_n_frame, mission_id_);
         map->addVertex(vi_map::Vertex::UniquePtr(map_vertex));
         pose_graph::VertexIdList id_list;
